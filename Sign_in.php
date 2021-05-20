b35e2bfa2c22b60b62a28bf33056c4fd935f7819
john484848 <kloepj@rpi.edu>
2021-05-20 00:25:11 -0400

    Basic SQL work

--- Sign_in.php
+++ Sign_in.php
@@ -1,3 +1,22 @@
+
 <html>
-<?php echo $_POST['username'];?>
+<?php
+$dbOk = false;
+
+/* Create a new database connection object, passing in the host, username,
+   password, and database to use. The "@" suppresses errors. */
+@ $db = new mysqli('localhost', 'root', '', 'buggle');
+
+if ($db->connect_error) {
+  echo '<div class="messages">Could not connect to the database. Error: ';
+  echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+} else {
+  $dbOk = true;
+}
+//$sql='select id, username, password FROM user_list WHERE username='.$_POST['username']. ' and password='.$_POST['pwd'] ;
+$result= $db->query("select * FROM user_list WHERE Username="."'".$_POST['username']."'"."and Password=" ."'".$_POST['pwd']."'");
+$row=$result->fetch_assoc();
+echo $row['id'];
+//echo $sql;
+?>
 </html>
